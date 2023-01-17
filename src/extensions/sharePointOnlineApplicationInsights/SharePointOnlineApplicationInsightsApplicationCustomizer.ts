7de02c324f4c7f4ee831f3db750a52b945b8a3d1
Francesco Bori <frbori@microsoft.com>
2023-01-17 12:08:03 +0100

    Formatting changes

--- src/extensions/sharePointOnlineApplicationInsights/SharePointOnlineApplicationInsightsApplicationCustomizer.ts
+++ src/extensions/sharePointOnlineApplicationInsights/SharePointOnlineApplicationInsightsApplicationCustomizer.ts
@@ -1,21 +1,13 @@
-import {
-  BaseApplicationCustomizer
-} from '@microsoft/sp-application-base';
+import { BaseApplicationCustomizer } from '@microsoft/sp-application-base';
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import { spfi, SPFx } from "@pnp/sp";
 import "@pnp/sp/webs";
 import "@pnp/sp/site-users/web";
 
-/**
- * If your command set uses the ClientSideComponentProperties JSON input,
- * it will be deserialized into the BaseExtension.properties object.
- * You can define an interface to describe it.
- */
 export interface ISharePointOnlineApplicationInsightsApplicationCustomizerProperties {
   connectionString: string;
 }
 
-/** A Custom Action which can be run during execution of a Client Side Application */
 export default class SharePointOnlineApplicationInsightsApplicationCustomizer
   extends BaseApplicationCustomizer<ISharePointOnlineApplicationInsightsApplicationCustomizerProperties> {
 
